d8cea9a44214d9f69e9ccf28ab6d20b7ec8b9a6c
Alex Bosworth <alex.bosworth@gmail.com>
2019-11-18 21:48:33 -0800

    improve cert parsing, add test

--- encryption/der_as_pem.js
+++ encryption/der_as_pem.js
@@ -2,6 +2,8 @@ const beginCert = '-----BEGIN CERTIFICATE-----';
 const beginKey = '-----BEGIN PUBLIC KEY-----';
 const endCert = '-----END CERTIFICATE-----';
 const endKey = '-----END PUBLIC KEY-----';
+const endMarker = 'ENDCERTIFICATE';
+const notFoundIndex = -1;
 const split = /.{0,64}/g;
 
 /** Get a DER encoded public key as a PEM encoded public key
@@ -18,10 +20,20 @@ const split = /.{0,64}/g;
 */
 module.exports = ({cert, key}) => {
   const base64Key = Buffer.from(cert || key, 'hex').toString('base64');
+  const endMarkLen = endMarker.length;
+
+  const endCertIndex = base64Key.indexOf(endMarker);
+  const keyLen = base64Key.length;
+
+  const hasEndMark = endCertIndex !== notFoundIndex;
+
+  const plusCount = keyLen - endCertIndex - endMarkLen;
+
+  const endCut = hasEndMark ? -(plusCount + plusCount + endMarkLen) : keyLen;
 
   const pem = []
     .concat(!cert ? beginKey : beginCert)
-    .concat(base64Key.match(split)).filter(n => !!n)
+    .concat(base64Key.slice(Number(), endCut).match(split).filter(n => !!n))
     .concat(!cert ? endKey : endCert)
     .join('\n');
 
