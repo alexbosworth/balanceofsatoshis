40de54eec03cd2c7eef943b4c82c6a842ac7d0ce
Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>
2022-02-07 16:42:42 -0500

    move to single file and broadcast tx separately.

--- peers/index.js
+++ peers/index.js
@@ -2,12 +2,10 @@ const findTagMatch = require('./find_tag_match');
 const interceptInboundChannels = require('./intercept_inbound_channels');
 const limitForwarding = require('./limit_forwarding');
 const openChannels = require('./open_channels');
-const openChannelsForSavedNodes = require('./open_channels_for_saved_nodes');
 
 module.exports = {
   findTagMatch,
   interceptInboundChannels,
   limitForwarding,
   openChannels,
-  openChannelsForSavedNodes,
 };
